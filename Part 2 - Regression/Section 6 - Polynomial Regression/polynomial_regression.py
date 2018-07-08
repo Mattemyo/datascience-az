ec50373a29a3ac5769f3714046aa4b5604346e89
Mattemyo <matias97marti@gmail.com>
2018-07-08 20:12:04 +0200

    :gem: refactor

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -29,7 +29,7 @@ lin_reg.fit(X, y)
 from sklearn.preprocessing import PolynomialFeatures
 poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
-poly_reg.fit(X_poly, y)
+train = poly_reg.fit(X_poly, y)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
@@ -43,7 +43,7 @@ plt.show()
 
 # Visualising the Polynomial Regression results
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.plot(X, lin_reg_2.predict(train), color = 'blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
