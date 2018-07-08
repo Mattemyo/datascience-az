c4887cfdd8bd82037c886825311af56a4d754c47
Mattemyo <matias97marti@gmail.com>
2018-07-08 18:57:14 +0200

    :bomb: do homework

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
+dataset = pd.read_csv(
+    r'C:\Users\Matias\Documents\datascience-az\Part 2 - Regression\Section 5 - Multiple Linear Regression\Multiple_Linear_Regression\50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
@@ -14,7 +15,7 @@ y = dataset.iloc[:, 4].values
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
 X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
@@ -22,7 +23,8 @@ X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -39,3 +41,23 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis=1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+# check which one to remove
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# remove idx 2 and repeat
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# remove idx 1 and repeat
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# remove idx 3 and repeat
+X_opt = X[:, [0, 4, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+# remove idx 4 and repeat
+X_opt = X[:, [0, 5]]
+regressor_OLS = sm.OLS(endog=y, exog=X_opt).fit()
+print(regressor_OLS.summary())
