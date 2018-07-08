f96dbe08826d4a2d2f78af496c435cffa23c502a
Mattemyo <matias97marti@gmail.com>
2018-07-08 15:16:48 +0200

    :rocket: use onehot encoding for categorical data

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -15,13 +15,27 @@ y = dataset.iloc[:, 3].values
 # taking care of missing data
 from sklearn.preprocessing import Imputer
 # axis = 0 ---> take mean of column
-imputer = Imputer(missing_values = 'NaN', strategy= 'mean', axis=0)
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
+# Encode categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# encode countries with onehotencoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+
+# encode yes/no with onehotencoder
+labelencoder_y = LabelEncoder()
+y = labelencoder_X.fit_transform(y)
+
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
