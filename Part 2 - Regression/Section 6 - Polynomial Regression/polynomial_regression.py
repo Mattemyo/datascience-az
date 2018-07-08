266e04d6ccd8a5227ba77a3c5c91f3393b9086a8
Mattemyo <matias97marti@gmail.com>
2018-07-08 20:24:51 +0200

    :art: finish regression part

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -6,7 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv(
+    r'C:\Users\Matias\Documents\datascience-az\Part 2 - Regression\Section 6 - Polynomial Regression\Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
