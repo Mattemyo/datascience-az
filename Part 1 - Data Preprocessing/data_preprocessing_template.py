d412cf973197c2e37932c4ab092f51e46dccd36b
Mattemyo <matias97marti@gmail.com>
2018-07-08 16:18:14 +0200

    :zap: clean up

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -12,25 +12,6 @@ dataset = pd.read_csv(
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# taking care of missing data
-from sklearn.preprocessing import Imputer
-# axis = 0 ---> take mean of column
-imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-
-# Encode categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-# encode countries with onehotencoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features=[0])
-X = onehotencoder.fit_transform(X).toarray()
-
-# encode yes/no with onehotencoder
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
